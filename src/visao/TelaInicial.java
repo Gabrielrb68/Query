7eab69f74b29fd08e773ee1c984bdbce27e26618
Bernardo <benckebernardo@gmail.com>
2023-02-09 10:22:09 -0300

    Tela Inicial

--- src/visao/TelaInicial.java
+++ src/visao/TelaInicial.java
@@ -2,15 +2,22 @@ package visao;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
 import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class TelaInicial extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtCpf;
+	private JTextField txtSenha;
 
 	/**
 	 * Launch the application.
@@ -41,6 +48,52 @@ public class TelaInicial extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 81, 81));
+		panel.setBounds(242, 0, 339, 530);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login Secretaria");
+		lblNewLabel.setBounds(70, 74, 201, 41);
+		lblNewLabel.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 30));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		panel.add(lblNewLabel);
+		
+		txtCpf = new JTextField();
+		txtCpf.setForeground(new Color(160, 160, 160));
+		txtCpf.setToolTipText("");
+		txtCpf.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 16));
+		txtCpf.setText("CPF");
+		txtCpf.setBounds(96, 212, 150, 30);
+		panel.add(txtCpf);
+		txtCpf.setColumns(10);
+		
+		txtSenha = new JTextField();
+		txtSenha.setForeground(new Color(160, 160, 160));
+		txtSenha.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 16));
+		txtSenha.setText("Senha");
+		txtSenha.setColumns(10);
+		txtSenha.setBounds(96, 251, 150, 30);
+		panel.add(txtSenha);
+		
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 17));
+		btnNewButton.setBackground(new Color(0, 81, 81));
+		btnNewButton.setBounds(114, 311, 116, 30);
+		panel.add(btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(64, 128, 128));
+		panel_1.setBounds(145, 0, 97, 530);
+		contentPane.add(panel_1);
+		
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setBackground(new Color(64, 128, 128));
+		panel_1_1.setBounds(581, 0, 97, 530);
+		contentPane.add(panel_1_1);
 	}
-
 }
